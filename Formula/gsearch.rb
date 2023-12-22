8ef18ad4a0ed38b9f8940bdbe15ea7ef8f13c466
Jianshu_Zhao <38149286+jianshu93@users.noreply.github.com>
2023-12-22 05:06:03 -0500

    new release 0.1.7

--- Formula/gsearch.rb
+++ Formula/gsearch.rb
@@ -4,9 +4,9 @@
 class Gsearch < Formula
   desc "Ultra-fast and Scalable Microbial Genome Search Program based on Various MinHash-like Metrics and HNSW written in Rust"
   homepage "https://github.com/jean-pierreBoth/gsearch"
-  url "https://github.com/jianshu93/gsearch/releases/download/v0.1.6/GSearch_macos_universal_v0.1.3.tar.gz"
-  sha256 "efa5de878a3cae9cc9b395edd4337033fb67f1cd0a634eb938d05448886e5ecb"
-  version "0.1.6"
+  url "https://github.com/jianshu93/gsearch/releases/download/v0.1.7/GSearch_macos_universal_v0.1.3.tar.gz"
+  sha256 "275f89ff36b083efe42ab1b107c71d8be3ba9bd627f772cc63eece4118cf7c06"
+  version "0.1.7"
 
   def install
     bin.install "gsearch"
