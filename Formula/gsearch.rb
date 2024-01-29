1882ccc70d89af406067bb2e4dfc7cec32c7ab76
Jianshu_Zhao <38149286+jianshu93@users.noreply.github.com>
2024-01-28 23:11:24 -0500

    Update to gsearch v0.1.9

--- Formula/gsearch.rb
+++ Formula/gsearch.rb
@@ -2,11 +2,11 @@
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Gsearch < Formula
-  desc "Ultra-fast and Scalable Microbial Genome Search Program based on Various MinHash-like Metrics and HNSW written in Rust"
+  desc "Ultra-fast and Scalable Genome Search Program based on Various MinHash-like Metrics and HNSW written in Rust"
   homepage "https://github.com/jean-pierreBoth/gsearch"
   url "https://github.com/jianshu93/gsearch/releases/download/v0.1.8/GSearch_macos_universal_v0.1.3.tar.gz"
-  sha256 "e2ee95c8dfa01d3407838323e0f0f7ad27177e6eb57d077a393ffc475617831f"
-  version "0.1.8"
+  sha256 "01a204c7cbd5511c1f857ad6c319de6ec7c18c62c088de4d9790df6e4a6335a2"
+  version "0.1.9"
 
   def install
     bin.install "gsearch"
