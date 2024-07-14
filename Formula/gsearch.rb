f1f5960bcf57ec0838a649b589056f877115b748
Jianshu_Zhao <38149286+jianshu93@users.noreply.github.com>
2024-07-14 19:57:26 -0400

    Update gsearch v0.1.4
    add supernal

--- Formula/gsearch.rb
+++ Formula/gsearch.rb
@@ -4,12 +4,13 @@
 class Gsearch < Formula
   desc "Ultra-fast and Scalable Genome Search Program based on Various MinHash-like Metrics and HNSW written in Rust"
   homepage "https://github.com/jean-pierreBoth/gsearch"
-  url "https://github.com/jianshu93/gsearch/releases/download/v0.1.9/GSearch_macos_universal_v0.1.3.tar.gz"
-  sha256 "c7b5586c76e61bbbf3d32d17cae88a960fd261e4df70332986549283ed691979"
-  version "0.1.9"
+  url "https://github.com/jean-pierreBoth/gsearch/releases/download/v0.1.5/GSearch_Darwin_universal_v0.1.4.tar.gz"
+  sha256 "11b75a478faded24e17a334631b1664c3bebad1aebc259db8a57b79c6bce061b"
+  version "0.2.0"
 
   def install
     bin.install "gsearch"
     bin.install "reformat"
+    bin.install "superani"
   end
 end
