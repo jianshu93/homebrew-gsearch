e02bfdca9383bd97ceaa46594f027f327b582a40
Jianshu_Zhao <38149286+jianshu93@users.noreply.github.com>
2024-08-06 15:30:00 -0400

    Update gsearch.rb

--- Formula/gsearch.rb
+++ Formula/gsearch.rb
@@ -5,7 +5,7 @@ class Gsearch < Formula
   desc "Ultra-fast and Scalable Genome Search Program based on Various MinHash-like Metrics and HNSW written in Rust"
   homepage "https://github.com/jean-pierreBoth/gsearch"
   url "https://github.com/jianshu93/gsearch/releases/download/v0.1.5/gsearch_Darwin_universal_v0.1.5.tar.gz"
-  sha256 "4ceb5a17f2eb0a95bb9fcbb74861d3253801acc47c24d8b5b1d5dfcce956a423"
+  sha256 "9017bacbdc4a6954a663785d307ac4d88898d9a252bfe5dc0e0a18944c4e48f7"
   version "0.2.1"
 
   def install
