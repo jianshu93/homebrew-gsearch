cafe16668fda3171ce722d8c95d89b6d9764546e
Jianshu_Zhao <38149286+jianshu93@users.noreply.github.com>
2024-08-06 15:13:40 -0400

    Update to v0.1.5
    update gsearch to v0.1.5 with more functionalities added.

--- Formula/gsearch.rb
+++ Formula/gsearch.rb
@@ -4,13 +4,17 @@
 class Gsearch < Formula
   desc "Ultra-fast and Scalable Genome Search Program based on Various MinHash-like Metrics and HNSW written in Rust"
   homepage "https://github.com/jean-pierreBoth/gsearch"
-  url "https://github.com/jean-pierreBoth/gsearch/releases/download/v0.1.5/GSearch_Darwin_universal_v0.1.4.tar.gz"
-  sha256 "11b75a478faded24e17a334631b1664c3bebad1aebc259db8a57b79c6bce061b"
-  version "0.2.0"
+  url "https://github.com/jianshu93/gsearch/releases/download/v0.1.5/gsearch_Darwin_universal_v0.1.5.tar.gz"
+  sha256 "4ceb5a17f2eb0a95bb9fcbb74861d3253801acc47c24d8b5b1d5dfcce956a423"
+  version "0.2.1"
 
   def install
     bin.install "gsearch"
     bin.install "reformat"
     bin.install "superani"
+    bin.install "superaai"
+    bin.install "hnswcore"
+    bin.install "FragGeneScanRs"
+    bin.install "hmmsearch"
   end
 end
