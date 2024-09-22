f37ff78f145926c1b3cbf00ac201637e7d9a3409
Jianshu_Zhao <38149286+jianshu93@users.noreply.github.com>
2024-09-22 17:06:24 -0400

    Update gsearch to 0.2.2
    add gsearch v0.2.2 with bigsig

--- Formula/gsearch.rb
+++ Formula/gsearch.rb
@@ -4,7 +4,7 @@
 class Gsearch < Formula
   desc "Ultra-fast and Scalable Genome Search Program based on Various MinHash-like Metrics and HNSW written in Rust"
   homepage "https://github.com/jean-pierreBoth/gsearch"
-  url "https://github.com/jianshu93/gsearch/releases/download/v0.1.5/gsearch_Darwin_universal_v0.1.5.tar.gz"
+  url "https://github.com/jianshu93/gsearch/releases/download/v0.2.1/gsearch_Darwin_universal_v0.1.5.tar.gz"
   sha256 "8823813f6346d0d7cdc438db64865a0d9162120e4972c0d7ef3754d7bb8c16fe"
   version "0.2.2"
 
@@ -16,5 +16,7 @@ class Gsearch < Formula
     bin.install "hnswcore"
     bin.install "FragGeneScanRs"
     bin.install "hmmsearch_rs"
+    bin.install "bigsig"
+    
   end
 end
